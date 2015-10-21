1b1e93859e459e368eceabfed25626e1e73cf1a7
balvarezescudero <balvarezescudero@atanasoff30.danielcastelao.org>
2015-10-21 11:03:59 +0200

    clase

--- src/bol5/pkg3/Circulo.java
+++ src/bol5/pkg3/Circulo.java
@@ -10,24 +10,23 @@ package bol5.pkg3;
  * @author Admin
  */
 public class Circulo {
-    public double radio;
-    private final double Pi=3.14;
+    private double radio;
+    public final double Pi=3.14;
     public Circulo (){
     
 }
     public Circulo(double radio){
-      
         this.radio=radio;
     }
-    public void setRadio(double ratio){
-        double r = 0;
-        this.radio=r;
+    public void setRadio(double radio){
+       
+        this.radio=radio;
     }
     public double getRadio(){
         return radio;
     }
     public double calArea(){
-        return Pi*Math.pow(radio,2);
+        return Pi*(Math.pow(radio,2));
     }
     public double calLonxitude(){
         return 2*Pi*radio;
